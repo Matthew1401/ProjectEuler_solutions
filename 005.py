32de3bba914e28dc5f7604b5cacca20c82607d95
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-05-30 12:20:30 +0200

    Solution

--- 005.py
+++ 005.py
@@ -1,16 +1,4 @@
-num = 2520
-
-is_loop = True
-while is_loop:
-    num += 17
-    this_is_it = True
-    for i in range(1, 20):
-        if num % i != 0:
-            this_is_it = False
-
-    if this_is_it:
-        break
-
-    print(num)
-
+num = 1
+for i in range(1, 21):
+    num *= i
 print(num)
